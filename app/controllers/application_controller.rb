954d2b5708499016f5832d61294b0ad9bf8aa8cb
Daniel Levenson <dlev@smartlogicsolutions.com>
2012-06-18 11:44:29 -0400

    Reformatted comment

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,10 +4,11 @@ class ApplicationController < ActionController::Base
   private
 
     def current_cart
-      #session[:cart_id] returns the session for the current cart id,
-      # this argument is used to find the cart object of the session which matches said cart id
+      # session[:cart_id] returns the session for the current cart id,
+        # this argument is used to find the cart object of the session which matches said cart id
       Cart.find(session[:cart_id])
-    rescue ActiveRecord::RecordNotFound #assuming this line means: if no cart is returned from the line:9, run block:11-13
+      # assuming this line means if no cart is returned from the line:9, run the rescue block.
+    rescue ActiveRecord::RecordNotFound
       cart = Cart.create
       session[:cart_id] = cart.id #the session's cart id is the new cart's id
       cart #returns the cart
